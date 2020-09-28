10864308be23d76eef1fd9884b7930b2c1b7fe32
mtfuji3776 <32.allegro.con.brio.ed.appassionato@gmail.com>
2020-09-28 22:21:19 +0900

    pgfバックエンド導入

--- src/Tutorial.hs
+++ src/Tutorial.hs
@@ -9,44 +9,48 @@ import Diagrams.TwoD.Size
 import Diagrams.TwoD.Layout.Tree
 import Data.Tree
 import qualified Data.Set as Set
+import Data.Time
+import Diagrams.TwoD.Vector
 
 
 myCircle :: Diagram B
 myCircle = circle 1
 
 --easyRender :: (Show n, RealFloat n) => FilePath -> QDiagram SVG V2 n Any -> IO ()
-easyRender path diag = renderPretty path fixedSize diag
+easyRender name diag = renderPretty ("/Users/fujimotomakoto/Documents/latexs/DailyStrategy/202009/img/" ++ name) fixedSize diag
 
 setSize :: Num n => n -> n -> SizeSpec V2 n
 setSize w h = mkSizeSpec2D (Just w) (Just h)
 
 fixedSize :: Num n => SizeSpec V2 n
-fixedSize = setSize 400 400
+fixedSize = setSize 400 300
 
 
 -- Algebraic Graph
-data Graph = Empty | Vertex Int | Connect Graph Graph | Overlay Graph Graph deriving (Show)
+data Graph a = Empty | Vertex a | Connect (Graph a) (Graph a) | Overlay (Graph a) (Graph a) deriving (Show)
 
 
-instance Num Graph where
+instance Functor Graph where
+    fmap f Tutorial.Empty = Tutorial.Empty
+    fmap f (Vertex x) = Vertex (f x)
+    fmap f (Connect g1 g2) = Connect (fmap f g1) (fmap f g2)
+    fmap f (Overlay g1 g2) = Overlay (fmap f g1) (fmap f g2)
+
+instance Num a => Num (Graph a) where
     fromInteger n = let m = fromInteger n in Vertex m
     (+) = Overlay
     (*) = Connect
     abs = id
-    negate g = 
-        case g of Tutorial.Empty -> Tutorial.Empty;
-                  Vertex x -> Vertex (negate x);
-                  Connect g1 g2 -> Connect (negate g1) (negate g2);
-                  Overlay g1 g2 -> Overlay (negate g1) (negate g2)
+    negate = fmap negate
     signum = id
 
-toVertices :: Graph -> Set.Set Int
+toVertices :: Ord a => Graph a -> Set.Set a
 toVertices Tutorial.Empty = Set.empty
 toVertices (Vertex n) = Set.singleton n
 toVertices (Connect g1 g2) = Set.union (toVertices g1) (toVertices g2)
 toVertices (Overlay g1 g2) = Set.union (toVertices g1) (toVertices g2)
 
-toEdges :: Graph -> Set.Set (Int,Int)
+toEdges :: Ord a => Graph a -> Set.Set (a,a)
 toEdges Tutorial.Empty = Set.empty
 toEdges (Vertex n) = Set.empty
 toEdges (Overlay g1 g2) = Set.union (toEdges g1) (toEdges g2)
@@ -55,12 +59,12 @@ toEdges (Connect g1 g2) = Set.union (toEdges g1) (toEdges g2) <> Set.fromList [(
 univProd :: (a -> b) -> (a -> c) -> a -> (b,c)
 univProd f g x = (f x,g x)
 
-destruct :: Graph -> ([Int],[(Int,Int)])
+destruct :: Ord a => Graph a -> ([a],[(a,a)])
 destruct = univProd (Set.toList . toVertices) (Set.toList . toEdges)
 
-genRose :: Graph -> Int -> Tree Int
-genRose Tutorial.Empty x = Node x []
-genRose g x = let (vs,es) = destruct g; vs' = map snd $ filter (\t -> fst t == x) es in Node x $ map (genRose g) vs'
+genRose :: Ord a => a -> Graph a -> Tree a
+genRose x Tutorial.Empty  = Node x []
+genRose x g = let (vs,es) = destruct g; vs' = map snd $ filter (\t -> fst t == x) es in Node x $ map (flip genRose g) vs'
 
 -- 以下、チュートリアルのexampleたち
 main = mainWith myCircle
@@ -78,4 +82,10 @@ sidebyside = circle 1 ||| square 2
 ontop = circle 1 === square 2
 
 hcat_vcat = let circles = hcat (map circle [1..6]) in vcat $ replicate 3 circles --半径1~6の円が水平(horizontal)に6つ並んだものが、縦に3つ並んで描画される
+
+expvect = lwG 0.05 . mconcat . map fromOffsets $ [ [r *^ e (r @@ rad)] | r <- [33 * tau/32, 34 * tau/32 .. 2 * tau]]
+
+vectopr1 =
+    let vs = take 33 . iterate (scale (2**(1/32)) . rotateBy (1/32) $ unitX)
+    in mconcat $ map fromOffsets (map (:[]) vs)
 
